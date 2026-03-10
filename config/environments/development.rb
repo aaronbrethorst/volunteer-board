caa87c019e757d5e90bd69c69e975a65999e11eb
Aaron Brethorst <aaron@brethorsting.com>
2026-03-10 16:18:49 -0700

    Capture sent email with Mailhog in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,14 +31,14 @@ Rails.application.configure do
   # Store uploaded files on Cloudflare R2 (see config/storage.yml for options).
   config.active_storage.service = :r2
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
-
-  # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: "localhost",
+    port: 1025
+  }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
